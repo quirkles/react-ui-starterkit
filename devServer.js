7ecda974c86ffe2d2531cd084732732e4778fddf
quirkles <al.quirk@gmail.com>
2020-04-26 22:17:44 -0400

    Fix hook

--- devServer.js
+++ devServer.js
@@ -2,6 +2,7 @@ const WebpackDevServer = require('webpack-dev-server');
 const webpack = require('webpack');
 
 const config = require('./webpack.config.dev.js');
+
 const options = config.devServer;
 
 WebpackDevServer.addDevServerEntrypoints(config, options);
@@ -10,5 +11,6 @@ const devServer = new WebpackDevServer(compiler, options);
 
 const port = process.env.PORT || 5000;
 devServer.listen(port, 'localhost', () => {
+  // eslint-disable-next-line no-console
   console.log(`dev devServer listening on port ${port}`);
 });
